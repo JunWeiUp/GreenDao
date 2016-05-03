0d1870620b9d1aa2593f86ba8f9d5f6dcaca71cb
jinxin <jinxin@seeyouabroad.org>
2016-05-03 18:18:30 +0800

    修改地址

---
+++ app/src/main/java/com/example/junweiup/greendaodemo/BaseApplication.java
@@ -0,0 +1,44 @@
+package com.example.junweiup.greendaodemo;
+
+import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.junweiup.greendaodem.DaoMaster;
+import com.example.junweiup.greendaodem.DaoSession;
+
+/**
+ * Created by Administrator on 2016/5/3.
+ */
+public class BaseApplication extends Application {
+
+    private DaoMaster.DevOpenHelper helper;
+    private SQLiteDatabase db;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupDatabase();
+    }
+
+    private void setupDatabase() {
+        // 通过 DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
+        // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为 greenDAO 已经帮你做了。
+        // 注意：默认的 DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
+        // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
+        helper = new DaoMaster.DevOpenHelper(this, "note", null);
+        db = helper.getWritableDatabase();
+        // 注意：该数据库连接属于 DaoMaster，所以多个 Session 指的是相同的数据库连接。
+        daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
+
+    public SQLiteDatabase getDb() {
+        return db;
+    }
+}
---
+++ app/src/main/java/com/example/junweiup/greendaodemo/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.junweiup.greendaodemo;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import com.example.junweiup.greendaodem.Note;
+import com.example.junweiup.greendaodem.NoteDao;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+
+import de.greenrobot.dao.AbstractDao;
+import de.greenrobot.dao.query.Query;
+import de.greenrobot.dao.query.QueryBuilder;
+
+public class MainActivity extends ListActivity {
+    private EditText editText;
+    private Cursor cursor;
+    public static final String TAG = "DaoExample";
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String textColumn = NoteDao.Properties.Text.columnName;
+        String orderBy = textColumn + " COLLATE LOCALIZED ASC";
+        cursor = getDb().query(getNoteDao().getTablename(), getNoteDao().getAllColumns(), null, null, null, null, orderBy);
+        String[] from = {textColumn, NoteDao.Properties.Comment.columnName};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, from,
+                to);
+        setListAdapter(adapter);
+
+        editText = (EditText) findViewById(R.id.editTextNote);
+    }
+
+    /**
+     * Button 点击的监听事件
+     *
+     * @param view
+     */
+    public void onMyButtonClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonAdd:
+                addNote();
+                break;
+            case R.id.buttonQuery:
+                search();
+                break;
+            default:
+                ToastUtils.show(getApplicationContext(), "What's wrong ?");
+                break;
+        }
+    }
+
+    private void search() {
+        String noteText = editText.getText().toString();
+        editText.setText("");
+        if (noteText == null || noteText.equals("")) {
+            ToastUtils.show(getApplicationContext(), "Please enter a note to query");
+        } else {
+            // Query 类代表了一个可以被重复执行的查询
+            Query query = getNoteDao().queryBuilder()
+                    .where(NoteDao.Properties.Text.eq(noteText))
+                    .orderAsc(NoteDao.Properties.Date)
+                    .build();
+            // 查询结果以 List 返回
+            List notes = query.list();
+            ToastUtils.show(getApplicationContext(), "There have " + notes.size() + " records");
+        }
+        // 在 QueryBuilder 类中内置两个 Flag 用于方便输出执行的 SQL 语句与传递参数的值
+        QueryBuilder.LOG_SQL = true;
+        QueryBuilder.LOG_VALUES = true;
+//        Query<Note> query = getNoteDao().queryBuilder()
+//                .where(NoteDao.Properties.Text.eq(noteText))
+//                .build();
+//        List<Note> list = query.list();
+//        Toast.makeText(NoteActivity.this, "text"+list.size(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void addNote() {
+        String noteText = editText.getText().toString();
+        editText.setText("");
+
+        final DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+        String comment = "Added on " + df.format(new Date());
+
+        if (noteText == null || noteText.equals("")) {
+            ToastUtils.show(getApplicationContext(), "Please enter a note to add");
+        } else {
+            // 插入操作，简单到只要你创建一个 Java 对象
+            Note note = new Note(null, noteText, comment, new Date());
+            getNoteDao().insert(note);
+            Log.d(TAG, "Inserted new note, ID: " + note.getId());
+            cursor.requery();
+        }
+    }
+
+    private SQLiteDatabase getDb() {
+        // 通过 BaseApplication 类提供的 getDb() 获取具体 db
+        return ((BaseApplication) this.getApplicationContext()).getDb();
+    }
+
+
+    private NoteDao getNoteDao() {
+        // 通过 BaseApplication 类提供的 getDaoSession() 获取具体 Dao
+        return ((BaseApplication) this.getApplicationContext()).getDaoSession().getNoteDao();
+    }
+
+
+
+    /**
+     * ListView 的监听事件，用于删除一个 Item
+     *
+     * @param l
+     * @param v
+     * @param position
+     * @param id
+     */
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // 删除操作，你可以通过「id」也可以一次性删除所有
+        getNoteDao().deleteByKey(id);
+//        getNoteDao().deleteAll();
+        ToastUtils.show(getApplicationContext(), "Deleted note, ID: " + id);
+        Log.d(TAG, "Deleted note, ID: " + id);
+        cursor.requery();
+    }
+}
